fb48bd2c40c289a590a99d93c27a395f5b403b3c
Divine Ntambwe <divinentambwe28@gmaail.com>
2025-09-20 21:29:13 +0200

    using public IP

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -5,7 +5,11 @@ export const appContext = createContext();
 
 
 function AppContext({children}) {
-   const url = import.meta.env.VITE_ELASTIC_IP;
+  //  const url = import.meta.env.VITE_ELASTIC_IP;
+   const fullUrl = new URL(window.location);
+   const url = `http://${fullUrl.searchParams.get('ip')}:5000`;
+   window.history.pushState({}, '', fullUrl); // Updates URL without reloading
+   console.log(url)
 
    function handleOpenCart(cart){
     cart.style.display = "flex"
