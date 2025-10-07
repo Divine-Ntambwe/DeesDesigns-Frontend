6f57265e5981ce356d146250b8a121e5ffbfe740
Divine Ntambwe <divinentambwe28@gmaail.com>
2025-10-07 13:02:32 +0200

    changed elastic ip

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -5,12 +5,7 @@ export const appContext = createContext();
 
 
 function AppContext({children}) {
-  //  const url = import.meta.env.VITE_ELASTIC_IP;
-   const fullUrl = new URL(window.location);
-   const ip = fullUrl.searchParams.get('ip') || localStorage.getItem("ip");
-   localStorage.setItem("ip",ip);
-   const url = `http://${ip}:5000`;
-   window.history.pushState({}, '', fullUrl); // Updates URL without reloading
+   const url = import.meta.env.VITE_ELASTIC_IP;
 
    function handleOpenCart(cart){
     cart.style.display = "flex"
